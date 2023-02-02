6a026d9645c438e4895c8ae4c660252902c0b5ae
wladerer <wtladerer@gmail.com>
2023-02-01 20:19:31 -0800

    created a config script

---
+++ configure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#check if the VSHDIR variable is set
+if [ -z "$VSHDIR" ]; then
+    echo "VSHDIR is not set"
+    
+    #prompt the user to set the VSHDIR variable
+    echo "Please set the VSHDIR variable to the directory where you have cloned the vsh repository"
+    exit
+fi
+
+#checks to see if ~/.bashrc sources vsh/v.sh and vsh/.vshrc and if not, adds them
+if ! grep -q "source $VSHDIR/v.sh" ~/.bashrc; then
+    echo "source $VSHDIR/v.sh" >>~/.bashrc
+fi
+
+if ! grep -q "source $VSHDIR/.vshrc" ~/.bashrc; then
+    echo "source $VSHDIR/.vshrc" >>~/.bashrc
+fi
+
+#source the .bashrc file
+source ~/.bashrc
+
+
+
