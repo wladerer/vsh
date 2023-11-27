905aec7d8405c836f6c670f3e77e43e94a733f7d
wladerer <wtladerer@gmail.com>
2023-11-27 14:21:37 -0800

    Refactor lazy_import function

--- vsh/cli.py
+++ vsh/cli.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 import argparse
+import importlib
 from . import scripts
-import importlib.util
-import sys
 
 
 def lazy_import(name):
-    spec = importlib.util.find_spec(name)
-    loader = importlib.util.LazyLoader(spec.loader)
-    spec.loader = loader
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[name] = module
-    loader.exec_module(module)
+    module = importlib.import_module(name)
     return module
 
 
