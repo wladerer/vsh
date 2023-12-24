290cf63933e205d634749bf020ec5d8936d12a98
wladerer <wtladerer@gmail.com>
2023-12-24 11:06:12 -0800

    Add termination script for VASP calculations

---
+++ utils/termination.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#take file as input from command line
+file=$1
+
+vsh slab $file -m 0 0 1 -o slab
+vsh kpoints --mesh 9 9 1 -o KPOINTS
+vsh kpoints --plane 60 -o KPATH
+vsh incar --write slab -o geom.incar
+vsh incar --write band-slab -o band.incar
+vsh incar --write single-point-slab -o sod.incar
+vsh incar --write single-point-slab-soc -o soc.incar
+
+
+#for every .vasp file in the current directory, make a subdirectory
+#and copy the file into it
+
+for i in *.vasp; do
+    dir="${i%.vasp}"
+    mkdir -p "$dir"/band "$dir"/sod "$dir"/soc
+
+    rsync -av --exclude='*.vasp' --exclude='KPOINTS' --exclude='KPATH' --exclude='*.incar' "$i" "$dir"/POSCAR
+    rsync -av --exclude='KPATH' KPOINTS "$dir"
+    rsync -av KPOINTS "$dir"/sod
+    rsync -av KPOINTS "$dir"/soc
+    rsync -av KPATH "$dir"/band
+
+    #copy the INCAR files into the subdirectories
+    rsync -av geom.incar "$dir"/INCAR
+    rsync -av band.incar "$dir"/band/INCAR
+    rsync -av sod.incar "$dir"/sod/INCAR
+    rsync -av soc.incar "$dir"/soc/INCAR
+    
+done
