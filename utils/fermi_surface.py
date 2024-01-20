93b6a841c504d1438bc29cd4e0876af931da60ec
William Laderer <wtladerer@gmail.com>
2024-01-19 18:55:41 -0800

    Add functions for loading eigenvalues from pickle file, getting kpoints from vasprun file, and updating kpoint coordinates in the dataframe. Also, plot the bands in 3D.

--- utils/fermi_surface.py
+++ utils/fermi_surface.py
@@ -1,32 +1,60 @@
 import pandas as pd
+import numpy as np
+from pymatgen.io.vasp.outputs import Vasprun
+import pickle
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def get_fermi_bands(dataframe: pd.DataFrame, efermi: float, tolernace: float = 0.1):
+def projected_eigenvalues_from_pickle(file: str) -> pd.DataFrame:
+    """Loads eigenvalues from pickle file"""
+    with open(file, "rb") as file:
+        loaded_dict = pickle.load(file)
+    return loaded_dict
+
+def kpoints_coordinates_from_vasprun(vasprun: Vasprun) -> list[list[float,float,float]]:
+    '''Gets the kpoints from a vasprun file'''
+    kpoints: list[list[float,float,float]] = vasprun.actual_kpoints
+    return kpoints
+
+def get_fermi_band_indices(dataframe: pd.DataFrame, efermi: float, tolernace: float = 0.1) -> list[int]:
     """Gets the bands that are within the tolerance of the fermi level"""
     dataframe = dataframe[dataframe["Energy"] >= efermi - tolernace]
     dataframe = dataframe[dataframe["Energy"] <= efermi + tolernace]
     bands = dataframe["Band"].unique()
-    
     return bands
 
-def update_kpoint_coordinates(dataframe: pd.DataFrame, kpoint_coordinates: dict):
+def update_dataframe_kpoint_coordinates(dataframe: pd.DataFrame, kpoint_coordinates: list[list[float,float,float]]) -> pd.DataFrame:
     '''Updates the kpoint coordinates in the dataframe'''
-    dataframe['Kpoint'] = dataframe['Kpoint'].map(kpoint_coordinates)
+    #add Kx, Ky, Kz columns
+    dataframe["Kx"] = np.nan
+    dataframe["Ky"] = np.nan
+    dataframe["Kz"] = np.nan
+    for i, kpoint in enumerate(kpoint_coordinates):
+        dataframe.loc[dataframe["Kpoint"] == i, ["Kx", "Ky", "Kz"]] = kpoint
     return dataframe
 
-def get_fermi_surface_points(dataframe: pd.DataFrame, bands: list[int]):
+def get_fermi_surface_kpoint_indices(dataframe: pd.DataFrame, bands: list[int]) -> list[int]:
     '''Gets the kpoints that are on the fermi surface'''
     dataframe = dataframe[dataframe["Band"].isin(bands)]
     kpoints = dataframe["Kpoint"].unique()
     return kpoints
 
-def create_kpoint_mesh(dataframe: pd.DataFrame, kpoints: list[int]):
-    '''Creates a mesh grid of kpoints to plot the fermi surface'''
-    kpoint_coordinates = dict(zip(kpoints, range(len(kpoints))))
-    dataframe = update_kpoint_coordinates(dataframe, kpoint_coordinates)
-    nkpoints = dataframe['Kpoint'].nunique()
-    nbands = dataframe['Band'].nunique()
-    kpoint_mesh = np.zeros((nkpoints, nbands))
-    for kpoint in kpoints:
-        kpoint_data = dataframe[dataframe['Kpoint'] == kpoint]
-        kpoint_mesh[kpoint, kpoint_data['Band']] = kpoint_data['Energy']
-    return kpoint_mesh
+efermi = 2.0672
+eigs = projected_eigenvalues_from_pickle('/Users/wladerer/Downloads/bi2te3/bi2te3.pkl')
+kpoint_coords = kpoints_coordinates_from_vasprun(Vasprun('/Users/wladerer/Downloads/bi2te3/vasprun.xml', parse_dos=False, parse_eigen=False, parse_projected_eigen=False, parse_potcar_file=False))
+bands_indices = get_fermi_band_indices(eigs, efermi)
+eigs = update_dataframe_kpoint_coordinates(eigs, kpoint_coords)
+kpoint_indices = get_fermi_surface_kpoint_indices(eigs, bands_indices)
+
+# Plotting the bands in 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(eigs["Kx"], eigs["Ky"], eigs["Kz"], c=eigs["Energy"], cmap='viridis')
+ax.set_xlabel('k_x')
+ax.set_ylabel('k_y')
+ax.set_zlabel('k_z')
+plt.show()
+
+
+
+
