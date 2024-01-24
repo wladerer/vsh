b08eefaaec59d0e5cf1950a12b9899ab5f645dea
William Laderer <wtladerer@gmail.com>
2024-01-23 16:07:03 -0800

    Add orthogonalization utilities and SOAP descriptors

--- vsh/scripts/slab.py
+++ vsh/scripts/slab.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-import argparse
+
+'''Orthogonalization utilities were written by Dr. Yonghyuk Lee'''
+
 from pymatgen.core.structure import Structure
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.io.vasp.inputs import Poscar
+from pymatgen.io.ase import AseAtomsAdaptor
+import numpy as np
+from dscribe.descriptors import SOAP
+from sklearn.preprocessing import normalize
+from ase import Atoms
 
 
 def structure_from_file(filename: str) -> Structure:
@@ -18,6 +25,7 @@ def slab_from_structure(
     is_primitive: bool,
     center_slab: bool,
     in_unit_planes: bool,
+    reorient_lattice: bool,
 ) -> list[Structure]:
     slabgen = SlabGenerator(
         structure,
@@ -27,6 +35,7 @@ def slab_from_structure(
         primitive=is_primitive,
         center_slab=center_slab,
         in_unit_planes=in_unit_planes,
+        reorient_lattice=reorient_lattice,
     )
     return slabgen.get_slabs()
 
@@ -52,15 +61,96 @@ def freeze_slab(structure: Structure, min_z: float) -> Structure:
         else:
             site.properties["selective_dynamics"] = [True, True, True]
 
+def update_structure(
+                     traj,
+                     soap_info = {
+                                  'rcut':5,
+                                  'nmax':8,
+                                  'lmax':4,
+                                  }
+                     ):
+
+    species = []
+    if type(traj) == list:
+        for t in traj:
+            elements = np.unique(t.get_chemical_symbols())
+            for e in elements:
+                if e not in species:
+                    species.append(e)
+    elif type(traj) == Atoms:
+        elements = np.unique(traj.get_chemical_symbols())
+        for e in elements:
+            if e not in species:
+                species.append(e)
+
+    soap = SOAP(
+                species=species,
+                periodic=True,
+                r_cut=soap_info['rcut'],
+                n_max=soap_info['nmax'],
+                l_max=soap_info['lmax'],
+                )
+
+    if type(traj) == list:
+        for s in traj:
+            try:
+                del s.arrays['SOAP']
+            except:
+                pass
+            s.set_array('SOAP',normalize(soap.create(s)))
+    elif type(traj) == Atoms:
+            traj.set_array('SOAP',normalize(soap.create(traj)))
+
+    return traj
 
 
-def run(args):
+def quick_score(
+                test_structure,
+                training_structure,
+                ):
+    soap_test = test_structure.arrays['SOAP']
+    soap_train = training_structure.arrays['SOAP']
+    scores = np.dot(soap_train,soap_test.transpose())**2
+    score = np.min(np.max(scores,axis=0))
+    v = 2*(1-score)
+    if v < 10e-10:
+        score = 0 
+    else:
+        score = np.sqrt(v)
+    return score
+
+def orthogonalize_slab(file: str, alpha: float = None, beta: float = None, gamma: float = None):
+    """Orthogonalizes a structure"""
+    from ase.io import read
+    origin = read(file)
+    atoms = read(file)
+
+    cell = atoms.cell.standard_form()[0]
+    atoms.set_cell(cell, scale_atoms=True)
+
+    cell = atoms.cell.cellpar()
     
+    if alpha:
+        cell[3] = alpha
+    if beta:
+        cell[4] = beta
+    if gamma:
+        cell[5] = gamma
+
+    atoms.set_cell(cell)
+    atoms.wrap()
+
+    origin = update_structure(origin)
+    atoms = update_structure(atoms)
+
+    return atoms
+
+def gen_slabs(args):
+    '''Generate slabs using pymatgen slab generator'''
     if args.vacuum != 0:
         args.vacuum = 3 if args.in_unit_planes else args.vacuum
 
-    
-    structure = structure_from_file(args.structure)
+    structure = structure_from_file(args.input)
     slabs = slab_from_structure(
         structure=structure,
         miller_plane=args.miller_plane,
@@ -69,6 +159,7 @@ def run(args):
         is_primitive=args.primitive,
         center_slab=args.center_slab,
         in_unit_planes=args.in_unit_planes,
+        reorient_lattice=args.reorient_lattice,
     )
 
     if args.freeze:
@@ -76,12 +167,58 @@ def run(args):
             freeze_slab(slab, args.freeze)
 
     poscars = [slab_to_poscar(slab) for slab in slabs]
-    
+
     if not args.output:
         for poscar in poscars:
             print(poscar.get_str()) 
     else:
         for i, poscar in enumerate(poscars):
             poscar.write_file(f"{args.output}_{i}.vasp")
+
+
+def orthogonalize(args):
+    '''Orthogonalize a slab'''
+    from ase.io import read 
+    
+    orginal_atoms = read(args.input)
+    orthogonalized_atoms = orthogonalize_slab(args.input, args.alpha, args.beta, args.gamma)
+    
+    #convert to pymatgen structure
+    orthogonalized_structure = AseAtomsAdaptor.get_structure(orthogonalized_atoms)
+    poscar = Poscar(orthogonalized_structure, sort_structure=True)
+
+    if args.verbose:
+        orginal_atoms = update_structure(orginal_atoms)
+        orthogonalized_atoms = update_structure(orthogonalized_atoms)
+        print(f"Similarity score: {quick_score(orginal_atoms, orthogonalized_atoms)}")
+
+    if args.freeze:
+        freeze_slab(orthogonalized_structure, args.freeze)
+
+    if not args.output:
+        print(poscar.get_str())
+    else:
+        poscar.write_file(f'{args.output}')
+
+
+def run(args):
+    
+    if args.orthogonalize:
+
+        orthogonalize(args)
+
+    else:
+
+        gen_slabs(args)
+
+
+
+
+
+
+    
+
+
+
 
 
