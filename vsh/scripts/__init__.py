8b8dc833c7cb057e87aa8b3b3aa6b1322a414eb2
William Laderer <wtladerer@gmail.com>
2024-01-24 05:55:45 -0800

    Add directory argument to manage command

--- vsh/scripts/__init__.py
+++ vsh/scripts/__init__.py
@@ -176,8 +176,7 @@ def manage(subparsers):
     subp_manage.add_argument("-s", "--snapshot", type=str, help="Take perspective view images of a structure file")
     subp_manage.add_argument("-n", "--note", type=str, help="Add a note to the pickle file")
     subp_manage.add_argument("-e", "--electronic-structure", action="store_true", help="Add projected electronic structure to the pickle file")
-    
-
+    subp_manage.add_argument("-d", "--directory", type=str, default=".", help="Directory where the data is stored")
 
 def adsorb(subparsers):
     subp_adsorb = subparsers.add_parser("adsorb", help="Generate adsorbed structures")
