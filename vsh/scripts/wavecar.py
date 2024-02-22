c9dbea5911ff8a649c9fabdaf38cfa70a8009b64
wladerer <wtladerer@gmail.com>
2024-02-21 17:49:23 -0800

    Refactor Wavecar instantiation in generate_parchg function

--- vsh/scripts/wavecar.py
+++ vsh/scripts/wavecar.py
@@ -51,7 +51,7 @@ def get_partial_charge_density(
 
 def generate_parchg(args):
     """Generates a PARCHG file from a WAVECAR file."""
-    wave = Wavecar(args.input, args.vasp_type)
+    wave = Wavecar(args.input, vasp_type=args.vasp_type)
     poscar = Poscar.from_file(args.structure)
 
     # sanitize the input
